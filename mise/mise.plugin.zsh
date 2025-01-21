bcb7047073111c4e88e920a0861c80c5ad04cadb
kilianpaquier <kilian@kilianpaquier.com>
2025-01-21 16:05:34 +0000

    chore(mise): remove config file is NO_MISE_CONFIG is given
    Signed-off-by: kilianpaquier <kilian@kilianpaquier.com>

--- mise/mise.plugin.zsh
+++ mise/mise.plugin.zsh
@@ -1,6 +1,8 @@
 if (( ! $+commands[mise] )); then curl -fsSL https://mise.run | sh; fi
 
-if [ -z "$NO_MISE_ALIAS" ] && [ ! -f "$HOME/.config/mise/conf.d/mise.dotfiles.toml" ]; then
+if [ -n "$NO_MISE_CONFIG" ]; then
+  [ -f "$HOME/.config/mise/conf.d/mise.dotfiles.toml" ] && rm "$HOME/.config/mise/conf.d/mise.dotfiles.toml"
+elif [ ! -f "$HOME/.config/mise/conf.d/mise.dotfiles.toml" ]; then
 mkdir -p "$HOME/.config/mise/conf.d"
 cat << EOF > "$HOME/.config/mise/conf.d/mise.dotfiles.toml"
 [alias]
