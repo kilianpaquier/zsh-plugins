c002f7db5cc1455eaa73a87758a7c4f7b2269adb
kilianpaquier <git@kilianpaquier.com>
2025-08-11 10:36:57 +0000

    fix(docker): correctly handle user id
    Signed-off-by: kilianpaquier <git@kilianpaquier.com>

--- docker-rootless/docker-rootless.plugin.zsh
+++ docker-rootless/docker-rootless.plugin.zsh
@@ -4,4 +4,4 @@
 if (( ! $+commands[docker] )); then
   curl -fsSL https://get.docker.com | sh && dockerd-rootless-setuptool.sh install
 fi
-export DOCKER_HOST="unix:///run/user/1000/docker.sock"
+export DOCKER_HOST="unix:///run/user/$(id -u)/docker.sock"
