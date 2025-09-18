0b80c192ddc3bccde427b5e69a44fe30da1d8cf9
kilianpaquier <107955904+kilianpaquier@users.noreply.github.com>
2025-09-18 20:43:00 +0200

    chore(mise): remove default written config
    Signed-off-by: kilianpaquier <107955904+kilianpaquier@users.noreply.github.com>

--- mise/mise.plugin.zsh
+++ mise/mise.plugin.zsh
@@ -2,15 +2,3 @@
 # shellcheck disable=SC1071
 
 if (( ! $+commands[mise] )); then curl -fsSL https://mise.run | sh; fi
-
-if [ -n "$NO_MISE_CONFIG" ]; then
-  [ -f "$HOME/.config/mise/conf.d/mise.dotfiles.toml" ] && rm "$HOME/.config/mise/conf.d/mise.dotfiles.toml"
-elif [ ! -f "$HOME/.config/mise/conf.d/mise.dotfiles.toml" ]; then
-mkdir -p "$HOME/.config/mise/conf.d"
-cat << EOF > "$HOME/.config/mise/conf.d/mise.dotfiles.toml"
-[alias]
-craft = "ubi:kilianpaquier/craft"
-gitlab-storage-cleaner = "ubi:kilianpaquier/gitlab-storage-cleaner"
-go-builder-generator = "ubi:kilianpaquier/go-builder-generator"
-EOF
-fi
