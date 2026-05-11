d65fbbd1d57b335ce0ee7aa0f8b0ff6735df9fbc
kilianpaquier <git@kilianpaquier.dev>
2026-05-11 23:28:16 +0200

    chore(history): remove exclusions in history
    Signed-off-by: kilianpaquier <git@kilianpaquier.dev>

--- history/history.plugin.zsh
+++ history/history.plugin.zsh
@@ -12,5 +12,3 @@ setopt HIST_VERIFY           # Do not execute immediately upon history expansion
 setopt APPEND_HISTORY        # append to history file (Default)
 setopt HIST_NO_STORE         # Don't store history commands
 setopt HIST_REDUCE_BLANKS    # Remove superfluous blanks from each command line being added to the history.
-
-export HISTORY_IGNORE="(clear|export)*"
